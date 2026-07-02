fa95c9ec6e5266858be868581741a900b41fc1e7
MABO262001 <ballivian.miguel.dev@gmail.com>
2026-07-01 21:23:08 -0400

    sa

--- deploy/cpanel-index.php
+++ deploy/cpanel-index.php
@@ -6,6 +6,28 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 $basePath = '/home/grupo17sc/proyecto2_app';
+$baseUri = '/inf513/grupo17sc/proyecto2';
+
+
+$_SERVER['SCRIPT_NAME'] = $baseUri.'/index.php';
+$_SERVER['PHP_SELF'] = $baseUri.'/index.php';
+$_SERVER['SCRIPT_FILENAME'] = __FILE__;
+
+if (isset($_SERVER['REQUEST_URI'])) {
+    $requestUri = $_SERVER['REQUEST_URI'];
+    $queryString = '';
+
+    if (($queryPosition = strpos($requestUri, '?')) !== false) {
+        $queryString = substr($requestUri, $queryPosition);
+        $requestUri = substr($requestUri, 0, $queryPosition);
+    }
+
+    if ($requestUri === $baseUri || $requestUri === $baseUri.'/') {
+        $_SERVER['REQUEST_URI'] = '/'.$queryString;
+    } elseif (str_starts_with($requestUri, $baseUri.'/')) {
+        $_SERVER['REQUEST_URI'] = substr($requestUri, strlen($baseUri)).$queryString;
+    }
+}
 
 if (file_exists($maintenance = $basePath.'/storage/framework/maintenance.php')) {
     require $maintenance;
