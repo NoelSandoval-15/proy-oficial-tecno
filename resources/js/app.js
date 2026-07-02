781768f7c9c63abdf85e161be7aca1ac2fa08384
MABO262001 <ballivian.miguel.dev@gmail.com>
2026-07-01 20:38:09 -0400

    cambios

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,18 +10,11 @@ const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.vue`,
-            import.meta.glob('./Pages/**/*.vue')
-        ),
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue, {
-                ...window.Ziggy,
-                location: new URL(window.location.href),
-            })
+            .use(ZiggyVue)
             .mount(el);
     },
     progress: {
