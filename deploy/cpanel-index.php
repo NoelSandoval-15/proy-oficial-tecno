4521020a759a934955911064ab2ff823f5d3ed76
MABO262001 <ballivian.miguel.dev@gmail.com>
2026-07-01 21:27:05 -0400

    Agregar diagnostico de bootstrap cPanel

--- deploy/cpanel-index.php
+++ deploy/cpanel-index.php
@@ -6,41 +6,49 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 $basePath = '/home/grupo17sc/proyecto2_app';
-$baseUri = '/inf513/grupo17sc/proyecto2';
+$logFile = $basePath.'/storage/logs/cpanel-bootstrap.log';
 
+try {
+    ini_set('display_errors', '0');
+    ini_set('log_errors', '1');
+    ini_set('error_log', $logFile);
 
-$_SERVER['SCRIPT_NAME'] = $baseUri.'/index.php';
-$_SERVER['PHP_SELF'] = $baseUri.'/index.php';
-$_SERVER['SCRIPT_FILENAME'] = __FILE__;
+    error_reporting(E_ALL);
 
-if (isset($_SERVER['REQUEST_URI'])) {
-    $requestUri = $_SERVER['REQUEST_URI'];
-    $queryString = '';
-
-    if (($queryPosition = strpos($requestUri, '?')) !== false) {
-        $queryString = substr($requestUri, $queryPosition);
-        $requestUri = substr($requestUri, 0, $queryPosition);
+    if (file_exists($maintenance = $basePath.'/storage/framework/maintenance.php')) {
+        require $maintenance;
     }
 
-    if ($requestUri === $baseUri || $requestUri === $baseUri.'/') {
-        $_SERVER['REQUEST_URI'] = '/'.$queryString;
-    } elseif (str_starts_with($requestUri, $baseUri.'/')) {
-        $_SERVER['REQUEST_URI'] = substr($requestUri, strlen($baseUri)).$queryString;
-    }
+    require $basePath.'/vendor/autoload.php';
+
+    $app = require_once $basePath.'/bootstrap/app.php';
+
+    /*
+    |--------------------------------------------------------------------------
+    | Ajuste para subcarpeta cPanel
+    |--------------------------------------------------------------------------
+    */
+    $app->bind('path.public', function () {
+        return '/home/grupo17sc/proyecto2';
+    });
+
+    $kernel = $app->make(Kernel::class);
+
+    $response = $kernel->handle(
+        $request = Request::capture()
+    )->send();
+
+    $kernel->terminate($request, $response);
+} catch (Throwable $e) {
+    $message = '['.date('Y-m-d H:i:s').'] '
+        .$e::class.': '.$e->getMessage()
+        .' in '.$e->getFile().':'.$e->getLine()
+        .PHP_EOL.$e->getTraceAsString()
+        .PHP_EOL.PHP_EOL;
+
+    file_put_contents($logFile, $message, FILE_APPEND);
+
+    http_response_code(500);
+
+    echo 'Error interno. Revisar storage/logs/cpanel-bootstrap.log';
 }
-
-if (file_exists($maintenance = $basePath.'/storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-require $basePath.'/vendor/autoload.php';
-
-$app = require_once $basePath.'/bootstrap/app.php';
-
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
